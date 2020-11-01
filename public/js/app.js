4c0ae52f36ce3f737ac47292c0b7cc16dfda9eef
pamcuellas <pamcuellas@gmail.com>
2020-11-01 15:51:52 -0500

    'mapbox.streets' style removed from maps.

--- public/js/app.js
+++ public/js/app.js
@@ -158,20 +158,20 @@ const torontoCoordinates = [40.651070, -79.347015]; // Start the map with Toront
 	});
 	
 	// Define maps styles
+	/*
 	var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
 		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 		maxZoom: 18,
-//		id: "mapbox.streets",
-		id: "satellite-streets-v9",
+		id: "mapbox.streets",
 		accessToken: API_KEY
 	}).addTo( vMap ); // Add a basemap
-
+    */ 
 	var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 		maxZoom: 18,
 		id: "satellite-streets-v9",
 		accessToken: API_KEY
-	});
+	}).addTo( vMap );
 	
 	var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
 		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -196,7 +196,7 @@ const torontoCoordinates = [40.651070, -79.347015]; // Start the map with Toront
 
 	// Map styles options to appear in the control box.
 	let basemapControl = {
-		"Streets": streetmap,
+//		"Streets": streetmap,
 		"Satellite": satellite,
 		"Grayscale": light,
 		"Outdoors": outdoors,
