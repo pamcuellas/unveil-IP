17beeabbe456cc600552cc8816c5ffea8d5161c9
Paulo Mendoza Cuellas <pamcuellas@gmail.com>
2023-10-16 16:35:33 -0400

    Config adjusted again.

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();//instatiate environment variables
 
 let CONFIG = { app:{}, db:{} }; //Make this global to use all over the application
 
-CONFIG.app.app      		= '0.0.0.0'; //process.env.APP || '0.0.0.0';
+CONFIG.app.app      		= process.env.APP || '0.0.0.0';
 CONFIG.app.port     		= process.env.PORT;
 CONFIG.dialect      		= process.env.DB_DIALECT;
 		
@@ -11,10 +11,10 @@ CONFIG.db.host      		= process.env.DB_HOST;
 CONFIG.db.port      		= process.env.DB_PORT;
 CONFIG.db.database  		= process.env.DB_NAME;
 CONFIG.db.user      		= process.env.DB_USER;
+CONFIG.db.password  		= process.env.DB_PASSWORD;   
 CONFIG.db.connectTimeout	= process.env.DB_TIMEOUT;
 
 
-CONFIG.db.password  = process.env.DB_PASSWORD;   
 
 //CONFIG.jwt_encryption  = process.env.JWT_ENCRYPTION || 'jwt_please_change';
 //CONFIG.jwt_expiration  = process.env.JWT_EXPIRATION || '10000';
