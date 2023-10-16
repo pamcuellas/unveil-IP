bb51d0eb06cbce2c50a895001cbf2053760de7b0
Paulo Mendoza Cuellas <pamcuellas@gmail.com>
2023-10-16 16:04:14 -0400

    DB ConnectionTimeOut param added.

--- config/config.js
+++ config/config.js
@@ -3,14 +3,16 @@ require('dotenv').config();//instatiate environment variables
 
 let CONFIG = { app:{}, db:{} }; //Make this global to use all over the application
 
-CONFIG.app.app      = process.env.APP || '0.0.0.0';
-CONFIG.app.port     = process.env.PORT;
-CONFIG.dialect      = process.env.DB_DIALECT;
+CONFIG.app.app      		= process.env.APP || '0.0.0.0';
+CONFIG.app.port     		= process.env.PORT;
+CONFIG.dialect      		= process.env.DB_DIALECT;
+		
+CONFIG.db.host      		= process.env.DB_HOST;
+CONFIG.db.port      		= process.env.DB_PORT;
+CONFIG.db.database  		= process.env.DB_NAME;
+CONFIG.db.user      		= process.env.DB_USER;
+CONFIG.db.connectTimeout	= process.env.DB_TIMEOUT;
 
-CONFIG.db.host      = process.env.DB_HOST;
-CONFIG.db.port      = process.env.DB_PORT;
-CONFIG.db.database  = process.env.DB_NAME;
-CONFIG.db.user      = process.env.DB_USER;
 
 CONFIG.db.password  = process.env.DB_PASSWORD;   
 
