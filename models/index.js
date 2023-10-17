aee8285eef104a67e427a30579de4b20faeea96c
Paulo Mendoza Cuellas <pamcuellas@gmail.com>
2023-10-17 15:48:15 -0400

    .env removed

--- models/index.js
+++ models/index.js
@@ -5,6 +5,11 @@
 const CONFIG = require('../config/config');
 const mysql = require('mysql2');
 
+
+console.log("Connected to the host ==> " + CONFIG.db.host);
+
+
+
 const connection = mysql.createConnection(CONFIG.db);
 
 connection.connect(err => {
@@ -12,8 +17,8 @@ connection.connect(err => {
     console.error('An error occurred while connecting to the DB', err);
     throw err;
   }
-  console.log("Connected to the host ==> " + CONFIG.db.host);
-  //console.log("Connected to the database ==> " + CONFIG.db.database);
+  //console.log("Connected to the host ==> " + CONFIG.db.host);
+  console.log("Connected to the database ==> " + CONFIG.db.database);
 });
 
 module.exports = connection;
